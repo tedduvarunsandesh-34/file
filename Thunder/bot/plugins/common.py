25c8cfee37bab1c2fa22f7bc3e4513bc60d6faf7
tedduvarunsandesh-34 <tedduvarun376@gmail.com>
2025-12-03 20:14:58 +0530

    Add developer button to commands
    Added a developer button to the start and about commands for user interaction.

--- Thunder/bot/plugins/common.py
+++ Thunder/bot/plugins/common.py
@@ -102,7 +102,7 @@ async def start_command(bot: Client, msg: Message):
     btns = [
         [InlineKeyboardButton(MSG_BUTTON_GET_HELP, callback_data="help_command"),
          InlineKeyboardButton(MSG_BUTTON_ABOUT, callback_data="about_command")],
-        [InlineKeyboardButton(MSG_BUTTON_GITHUB, url="https://github.com/fyaz05/FileToLink/"),
+        [InlineKeyboardButton(MSG_BUTTON_DEVELOPER, url="https://t.me/ind_gamer_1"),
          InlineKeyboardButton(MSG_BUTTON_CLOSE, callback_data="close_panel")]
     ]
     
@@ -145,7 +145,7 @@ async def about_command(bot: Client, msg: Message):
     
     btns = [
         [InlineKeyboardButton(MSG_BUTTON_GET_HELP, callback_data="help_command")],
-        [InlineKeyboardButton(MSG_BUTTON_GITHUB, url="https://github.com/fyaz05/FileToLink/"),
+        [InlineKeyboardButton(MSG_BUTTON_DEVELOPER, url="https://t.me/ind_gamer_1"),
          InlineKeyboardButton(MSG_BUTTON_CLOSE, callback_data="close_panel")]
     ]
     
@@ -278,3 +278,4 @@ async def ping_command(bot: Client, msg: Message):
         )
     except MessageNotModified:
         pass
+
